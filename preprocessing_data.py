3bd0b29f54f752907a24fb4f52e23003f1e10362
unknown <doronharitan@gmail.com>
2019-11-11 12:54:45 +0200

    Add: dataset and dataloader function + split the data to test, train and validation + added transform function

--- preprocessing_data.py
+++ preprocessing_data.py
@@ -1,15 +1,9 @@
 import argparse
 import skvideo #can save the new data as jpeg, can be used once I change it to h5pq
 from tqdm import tqdm
-skvideo.setFFmpegPath(r"C:\Users\Doron\Documents\ffmpeg-20191106-fc7b6d5-win64-static\bin")
-import skvideo.io
+# skvideo.setFFmpegPath(r"C:\Users\Doron\Documents\ffmpeg-20191106-fc7b6d5-win64-static\bin")
+# import skvideo.io
 import os
-
-#used when saving the data into numpy array
-import numpy as np
-import h5py
-
-#used when saving the data into figuers
 import cv2
 
 
@@ -17,9 +11,7 @@ parser = argparse.ArgumentParser(description='UCF101 Action Recognition, LRCN ar
 parser.add_argument('--root', default=r'C:\Users\Doron\Desktop\ObjectRecognition\UCF101_row_data/', type=str, help='path to find the UCF101 row data')
 parser.add_argument('--ucf_list_root', default=r'C:\Users\Doron\PycharmProjects\ObjectRecognition\Data_UCF101\UCF101_video_list/', type=str, help='path to find the UCF101 list splitting the data to train and test')
 parser.add_argument('--sampling_rate', default=10, type=int, help='how to sample the data')
-parser.add_argument('--new_FPS', default='3', type=str, help='how to sample the data')
 parser.add_argument('--sampled_data_dir', default=r'C:\Users\Doron\Desktop\ObjectRecognition\UCF101_sampled_data', type=str, help='the default path for the new sampled data')
-parser.add_argument('--save_data_format', default='np', type=str, help='the default format to save the data in')
 
 class UCF101Preprocessing():
     """"
@@ -36,10 +28,7 @@ class UCF101Preprocessing():
         self.sampled_data_dir = arg.sampled_data_dir
         self.fbs = arg.new_FPS
         self.save_format = arg.save_data_format
-        if self.save_format == 'np':
-            self.folder_dir = self.sampled_data_dir + '_' + self.fbs + '_' + self.save_format
-        else:
-            self.folder_dir = self.sampled_data_dir + '_' + str(self.sampling_rate) + '_' + self.save_format
+        self.folder_dir = self.sampled_data_dir + '_' + str(self.sampling_rate) + '_' + self.save_format
 
 
     def create_sampled_data(self):
@@ -53,25 +42,14 @@ class UCF101Preprocessing():
         for file in os.listdir(self.ucf_list_root):
             if 'test' in file:
                 save_path = self.folder_dir + os.sep + 'test' + os.sep
-                dataset_name = 'test'
             elif 'train' in file:
                 save_path = self.folder_dir + os.sep + 'train' + os.sep
-                dataset_name = 'train'
             else:
                 continue
             with open(self.ucf_list_root + file) as f:
                 video_list = f.readlines()
             with tqdm(total=len(video_list)) as pbar:
                 for video_name in video_list:
-                    if self.save_format == 'np':
-                        video_file = video_name.split('\n')[0]
-                        if 'train' in file:
-                            video_file, label = video_file.split(' ')
-                            self.save_dataset(self.folder_dir + os.sep + 'train_label' + os.sep, 'label', video_file, label)
-                        video_data = skvideo.io.vread(self.root + video_file, outputdict={'-r': self.fbs})
-                        self.save_dataset(save_path, dataset_name, video_file,  video_data)
-                        pbar.update(1)
-                else:
                     video_name_save = video_name.split('.')[0].replace('/', ',')
                     video_data = cv2.VideoCapture(self.root + video_name.split(' ')[0])
                     frame_count = 0
@@ -83,9 +61,7 @@ class UCF101Preprocessing():
                         frame_count += 1
                     pbar.update(1)
 
-    def save_dataset(self, file_path, dataset_name, array_name, dataset):
-        with h5py.File(file_path + dataset_name + '.h5', 'a') as hf:
-            hf.create_dataset(array_name, data=dataset)
+
 
 if __name__ == '__main__':
     arg = parser.parse_args()
--- train.py
+++ train.py
@@ -3,7 +3,6 @@ import torch
 import argparse
 import os
 import torchvision
-import torchvision.transforms as trasforms
 from torch.utils.data import DataLoader
 from data import *
 
@@ -11,29 +10,41 @@ parser = argparse.ArgumentParser(description='UCF101 Action Recognition, LRCN ar
 parser.add_argument('--epochs', default=500, type=int, help='number of total epochs')
 parser.add_argument('--batch-size', default=16, type=int, help='mini-batch size (default: 32)')
 parser.add_argument('--lr', default=5e-4, type=float, help='initial learning rate')
-parser.add_argument('--num_workers', default=8, type=int, help='initial num_workers, the number of processes that generate batches in parallel')
-parser.add_argument('--sampled_data_path_train', default=r'C:\Users\Doron\Desktop\ObjectRecognition\UCF101_sampled_data_3_np\train', type=str
+parser.add_argument('--num_workers', default=1, type=int, help='initial num_workers, the number of processes that generate batches in parallel')
+parser.add_argument('--sampled_data_path', default=r'C:\Users\Doron\Desktop\ObjectRecognition\UCF101_sampled_data_10', type=str
                     , help='the path for the sampled row data')
 parser.add_argument('--ucf_list_root', default=r'C:\Users\Doron\PycharmProjects\ObjectRecognition\Data_UCF101\UCF101_video_list/', type=str, help='path to find the UCF101 list splitting the data to train and test')
-parser.add_argument('--data_format', default='np', type=str, help='the format the data is saved in')
+parser.add_argument('--num_frames_video', default=5, type=int, help='the number of frames that would be taken from each video')
+parser.add_argument('--seed', default=42, type=int, help='?')
+
 
 
 
 def main():
     args = parser.parse_args()
     print(args)
-    # open a new folder where all of the data including the tensorboard will be saved in
-    #set what is the device the code will run on (    device = torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-    #add a function that will help us understand the data a little bit, how many labels we have from each class, and etc.
-    #save all of the args in the folder
-    #add trasformores to the figure
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Todo: 1.open a new folder where all of the data including the tensorboard will be saved in
+    #  2. add a function that will help us understand the data a little bit, how many labels we have from each class, and etc.
+    #  3. save all of the args in the folder
+    #  4. add trasformores to the figure
 
-    transform = None
-    ucf101_dataset = UCF101Dataset(args.sampled_data_path_train, args.ucf_list_root, args.data_format, transform)
-    data_loader = torch.utils.data.DataLoader(args.sampled_data_path_train,
-                                          batch_size=args.batch_size,
-                                          shuffle=True,
-                                          num_workers=args.num_workers)
+    train_data, val_data, test_data  = split_data(args.ucf_list_root, args.seed)
+
+    train_dataset = UCF101Dataset(args.sampled_data_path, args.num_frames_video, train_data, mode = 'train')
+    data_loader = DataLoader(train_dataset, batch_size=args.batch_size,
+                                          shuffle=True) #how to shift it to multiprocessing?
+
+    val_dataset = UCF101Dataset(args.sampled_data_path, args.num_frames_video, val_data, mode='val')
+    data_loader = DataLoader(val_dataset, batch_size=args.batch_size,
+                             shuffle=True)  # how to shift it to multiprocessing?
+
+    test_dataset = UCF101Dataset(args.sampled_data_path, args.num_frames_video, test_data, mode='test')
+    data_loader = DataLoader(test_dataset, batch_size=args.batch_size,
+                             shuffle=True)  # how to shift it to multiprocessing?
+
+
+    #we need to have train_loader, val loader and test loader
 
 if __name__=='__main__':
     main()
